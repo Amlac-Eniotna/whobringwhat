4b2802e3a0adec78efb97a3afca47d4f876a3e21
Amlac Eniotna <antoine.calma30@gmail.com>
2025-12-16 12:00:28 +0100

    optimized browser

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  experimental: {
+    optimizePackageImports: ["lucide-react", "date-fns"],
+  },
 };
 
 export default nextConfig;
